ea09e27e6cdaacb91b0032ac381eac954492ecbc
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2024-12-08 16:54:51 +0700

    fix: error eslint node not found

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,20 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import daStyle from 'eslint-config-dicodingacademy';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import daStyle from "eslint-config-dicodingacademy";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   daStyle,
   {
     rules: {
-      'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
-  }},
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+      "linebreak-style": [
+        "error",
+        process.platform === "win32" ? "windows" : "unix",
+      ],
+    },
+    env: { node: true },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
