ad62b5e017af7e4ae89d57dfa7f4631c1b3b674f
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2025-02-20 13:34:59 +0700

    feat: implement local storage

---
+++ src/api/uploads/handler.js
@@ -0,0 +1,27 @@
+const autoBind = require('auto-bind');
+
+class UploadsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postUploadImageHandler(request, h) {
+    const { data } = request.payload;
+    this._validator.validateExportNotesPayload(data.hapi.headers);
+
+    const filename = await this._service.writeFile(data, data.hapi);
+    const response = h.response({
+      status: 'success',
+      data: {
+        fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = UploadsHandler;
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
+const path = require('path');
 
 // notes
 const notes = require('./api/notes');
@@ -29,6 +31,11 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+// uploads
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
+
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
@@ -36,6 +43,9 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(
+    path.resolve(__dirname, 'api/uploads/file/images')
+  );
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -44,6 +54,9 @@ const init = async () => {
       request: ['error'],
     },
     routes: {
+      payload: {
+        maxBytes: 1048576 * 10, // 10MB
+      },
       cors: {
         origin: ['*'],
       },
@@ -53,6 +66,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
@@ -110,6 +126,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
